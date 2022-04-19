310d137478fd50bb1b98a6f9412e47c2323ad55a
vicky911 <patelvicky1995@gmail.com>
2022-04-19 17:47:24 -0400

    Delete COMP6741_P1 directory

--- COMP6741_P1/datasets/Removeduplicate.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-data=pd.read_csv('course_data_before.csv', encoding='unicode_escape')
-data.drop_duplicates(keep='first',inplace=True)
-data.to_csv('courseData.csv',index=False)
